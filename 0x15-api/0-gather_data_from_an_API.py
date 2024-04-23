11330872486d84c250ba48708537044716833e4e
Tadala <naomikapengule@gmail.com>
2024-04-23 04:34:50 +0200

    Changed reference to task_list individual element

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,22 +7,22 @@ import sys
 
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com"
+    url = 'https://jsonplaceholder.typicode.com/'
 
-    user = "{}/users/{}".format(url, sys.argv[1])
-    response = RQ.get(user)
+    user_id = sys.argv[1]
+    employee = '{}users/{}'.format(url, user_id)
+    response = RQ.get(employee)
     json_data = response.json()
-    print("Employee {} is done with tasks".format(json_data.get('name')),
-          end="")
+    print("Employee {} is done with tasks".format(json_data.get('name')), end="")
 
-    todo = "{}/todos?userId={}".format(url, sys.argv[1])
-    response = RQ.get(todo)
+    todos = '{}todos?userId={}'.format(url, user_id)
+    response = RQ.get(todos)
     tasks = response.json()
     task_list = []
-    for el in tasks:
-        if el.get("completed") is True:
-            task_list.append(el)
+    for task in tasks:
+        if task.get('completed') is True:
+            task_list.append(task)
 
     print("({}/{}):".format(len(task_list), len(tasks)))
-    for el in task_list:
-        print("\t {}".format(tasks.get("title")))
+    for task in task_list:
+        print("\t {}".format(task.get("title")))
