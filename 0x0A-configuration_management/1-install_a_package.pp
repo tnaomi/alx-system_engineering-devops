027b21e8be4d152e7c54400a719f6ca55a0e4b3d
Tadala <naomikapengule@gmail.com>
2024-06-24 12:13:09 +0000

    Add class defn and quoted provider

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
 # Install a package: Flask
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => pip3,
+class pypackage{
+  package { 'flask':
+    ensure  => '2.1.0',
+    provider => 'pip3'
+  }
 }
