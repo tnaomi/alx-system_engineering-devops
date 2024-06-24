4068b6ffed80a586e9d4fcfea91960e1c34cd83f
Tadala <naomikapengule@gmail.com>
2024-06-24 12:28:36 +0000

    Quoted provider

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
 # Install a package: Flask
-class pypackage{
-  package { 'flask':
-    ensure  => '2.1.0',
-    provider=> 'pip3'
-  }
+package { 'flask':
+  ensure  => '2.1.0',
+  provider=> 'pip3'
 }
